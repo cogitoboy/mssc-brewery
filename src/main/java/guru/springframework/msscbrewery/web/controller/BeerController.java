c93b1e8eda52fd3fd83cc2feba86a9848ae6050a
cogitoboy <Cr@zy12345>
2022-05-08 10:54:05 -0600

    fixed the uuid issue

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -5,6 +5,7 @@ import guru.springframework.msscbrewery.web.services.BeerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +20,10 @@ public class BeerController {
     public BeerController(BeerService beerService) {
         this.beerService = beerService;
     }
-    @GetMapping("/")
-    public ResponseEntity<BeerDto> hello(){
-        System.out.println("hello");
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
-    }
 
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDto> getBeer(UUID beerId) {
+    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
+
 
         return new ResponseEntity(beerService.getBeerById(beerId), HttpStatus.OK);
     }
